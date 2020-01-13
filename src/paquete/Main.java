c6b79329cb7162fbf14ed350f3e0f3f879f4b4a1
Noelia Neira <noeliasneira@gmail.com>
2020-01-12 23:58:14 -0300

    Se agregan mas ejercicios con arrays

--- src/paquete/Main.java
+++ src/paquete/Main.java
@@ -27,9 +27,24 @@ public class Main {
 
        System.out.println(valorNum4);
 
+       int arrayNum5[] = {1, 2, 0, 7, 9};
 
+       System.out.println(arrayNum5[2]);
 
+       String arrayDeCaracteres[] = {"Noe", "Neira"};
 
+       System.out.println( "Mi nombre es: " + arrayDeCaracteres[0] +" " + arrayDeCaracteres[1]);
+
+       int arrayNum6[] = {1, 7, 3, 0, 1, 5};
+       System.out.println( "Este array contiene " + arrayNum6.length + " elementos");
+
+       String arrayDeCaracteres2[] = new String[4];
+       int size = arrayDeCaracteres2.length;
+       arrayDeCaracteres2 [0] = "Primero";
+       arrayDeCaracteres2 [size-1] = "Último";
+
+       System.out.println(arrayDeCaracteres2 [0]);
+       System.out.println(arrayDeCaracteres2 [size-1]);
 
     }
 }
