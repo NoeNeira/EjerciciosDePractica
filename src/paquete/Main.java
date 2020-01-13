3e21467bc150c3f1078032ff4444ca71740c1d04
Noelia Neira <noeliasneira@gmail.com>
2020-01-12 23:30:09 -0300

    Se agregan mas ejercicios básicos

--- src/paquete/Main.java
+++ src/paquete/Main.java
@@ -1,8 +1,35 @@
 package paquete;
 
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        String nombre = "Noelia";
+        String nombre = "Noe";
+
         System.out.println("Bienvenida" + " " + nombre);
+
+        String variable = "Primer Intento";
+        variable = "Segundo Intento";
+
+       System.out.println(variable);
+
+       int valorNum1 = 20;
+       int valorNum2 = 2;
+
+       System.out.println(valorNum1 + valorNum2);
+       System.out.println(valorNum1 - valorNum2);
+       System.out.println(valorNum1 * valorNum2);
+       System.out.println(valorNum1 / valorNum2);
+
+
+       int valorNum3 = 10;
+       int valorNum4 = valorNum3 + 1;
+
+       System.out.println(valorNum4);
+
+
+
+
+
     }
 }
